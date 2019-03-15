3df044847220426affc5a2463b76763b12b5e0aa
isjaki <usow.aleksey@yandex.ru>
2019-03-15 19:40:38 +0400

    created functional AddList componenent; added state to App component which contains following properties: lists array, as well as currentListName and currentListDate to create a new list with following name and date, values will be taken from the input elements in AddList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AddList from './components/AddList/AddList';
+
 class App extends Component {
+  state = {
+    currentListName: '',
+    currentListDate: '',
+    lists: []
+  }
+
   render() {
     return (
       <div className="App">
-     
+          <h1>TODO LIST PLANNER</h1>
+          <AddList />
+          <div>Lists</div>
       </div>
     );
   }
---
+++ src/components/AddList/AddList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const addList = props => (
+    <div>
+        <h2>Create a TODO list</h2>
+        <p>
+            <label htmlFor="name">Choose a name:</label>
+            <input id="name" name="name" type="text" />
+        </p>
+        <p>
+            <label htmlFor="date">Choose a date:</label>
+            <input id="date" name="date" type="date" />
+        </p>
+        <button>CREATE</button>
+    </div>
+);
+
+export default addList;
