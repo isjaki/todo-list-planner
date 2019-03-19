f1f45610255b17ff07b7f4984c96b8eb80578b4e
isjaki <usow.aleksey@yandex.ru>
2019-03-20 00:31:24 +0400

    added style to input in AddItem component

--- src/components/List/AddItem/AddItem.js
+++ src/components/List/AddItem/AddItem.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
+import './AddItem.css';
 
 const addItem = props => (
     <Aux>
-        <input type="text" />
+        <input 
+            type="text"
+            className="AddItemInput"
+            placeholder="What are you up to?" />
         <Button
             className="ListButton AddItem"
         >Add</Button>
