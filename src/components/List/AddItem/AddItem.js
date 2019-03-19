9cfb6fb7ea8140184981b7b6fc90384b603217a8
isjaki <usow.aleksey@yandex.ru>
2019-03-19 23:45:21 +0400

    styled delete and addItem buttons in List and AddItem components

--- src/components/List/AddItem/AddItem.js
+++ src/components/List/AddItem/AddItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const addItem = props => (
     <Aux>
         <input type="text" />
-        <button>Add</button>
+        <Button
+            className="ListButton AddItem"
+        >Add</Button>
     </Aux>
 );
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import ListItems from '../List/ListItems/ListItems';
 import AddItem from '../List/AddItem/AddItem';
+import Button from '../UI/Button/Button';
 import './List.css';
 
 class List extends Component {
@@ -10,7 +11,10 @@ class List extends Component {
             <div className="List">
                 <div>
                     <span>{this.props.title}</span><span>{this.props.date}</span>
-                    <button onClick={this.props.deleteList}>Delete</button>
+                    <Button 
+                        onClick={this.props.deleteList}
+                        className="ListButton Delete"
+                    >Delete</Button>
                 </div>
                 <ListItems />
                 <AddItem />
