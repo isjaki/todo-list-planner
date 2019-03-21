5e43d7e800a023137e529e704e6ccd20690bd092
isjaki <usow.aleksey@yandex.ru>
2019-03-22 01:02:53 +0400

    added function to toggle "hidden" class of the buttons in ListItem component

--- src/components/List/ListItems/ListItem/ListItem.js
+++ src/components/List/ListItems/ListItem/ListItem.js
@@ -4,18 +4,39 @@ import Button from '../../../UI/Button/Button';
 import './ListItem.css';
 
 class ListItem extends Component {
+    state = {
+        buttonsHidden: true
+    }
+
+    toggleButtonsClassHandler = () => {
+        this.setState(prevState => ({
+            buttonsHidden: !prevState.buttonsHidden
+        }));
+    }
+
     render() {
+        const completedButtonClasses = ['ListItemButton', 'Completed'];
+        const deleteButtonClasses = ['ListItemButton', 'Delete'];
+
+        if (this.state.buttonsHidden) {
+            completedButtonClasses.push('Hidden');
+            deleteButtonClasses.push('Hidden');
+        }
+
         return (
             <li className="ListItem">
                 {this.props.completed ? <i className="fas fa-check-square"></i> : <i className="fas fa-clock"></i>}
-                <span className="ListItem__TaskName">{this.props.taskName}</span>
+                <span
+                    className="ListItem__TaskName"
+                    onClick={this.toggleButtonsClassHandler}
+                >{this.props.taskName}</span>
                 <Button
-                    className="ListItemButton Completed Hidden"
+                    className={completedButtonClasses.join(' ')}
                 >
                     <i className="fas fa-check"></i>
                 </Button>
                 <Button
-                    className="ListItemButton Delete Hidden"
+                    className={deleteButtonClasses.join(' ')}
                 >
                     <i className="fas fa-times"></i>
                 </Button>
