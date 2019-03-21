ebc0885b19e64011953d14d489298fcf26584369
isjaki <usow.aleksey@yandex.ru>
2019-03-22 00:21:22 +0400

    added "hidden" class to the buttons in ListItem component, when applied, buttons hide behind a task name and dissappear, when removed, they move towards their places

--- src/components/List/ListItems/ListItem/ListItem.js
+++ src/components/List/ListItems/ListItem/ListItem.js
@@ -3,21 +3,24 @@ import React from 'react';
 import Button from '../../../UI/Button/Button';
 import './ListItem.css';
 
-const listItem = props => (
-    <li className="ListItem">
-        {props.completed ? <i className="fas fa-check-square"></i> : <i className="fas fa-clock"></i>}
-        <span className="ListItem__TaskName">{props.taskName}</span>
-        <Button
-            className="ListItemButton Completed Hidden"
-        >
-            <i className="fas fa-check"></i>
-        </Button>
-        <Button
-            className="ListItemButton Delete Hidden"
-        >
-            <i className="fas fa-times"></i>
-        </Button>
-    </li>
-);
+const listItem = props => {
+
+    return (
+       <li className="ListItem">
+            {props.completed ? <i className="fas fa-check-square"></i> : <i className="fas fa-clock"></i>}
+            <span className="ListItem__TaskName">{props.taskName}</span>
+            <Button
+                className="ListItemButton Completed Hidden"
+            >
+                <i className="fas fa-check"></i>
+            </Button>
+            <Button
+                className="ListItemButton Delete Hidden"
+            >
+                <i className="fas fa-times"></i>
+            </Button>
+        </li> 
+    );
+};
 
 export default listItem;
