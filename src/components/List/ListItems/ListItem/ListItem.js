5be35c8f3d52d2ef207b098596dfc016538eb954
isjaki <usow.aleksey@yandex.ru>
2019-03-21 23:03:07 +0400

    styled buttons in ListItem component

--- src/components/List/ListItems/ListItem/ListItem.js
+++ src/components/List/ListItems/ListItem/ListItem.js
@@ -7,8 +7,16 @@ const listItem = props => (
     <li className="ListItem">
         {props.completed ? <i className="fas fa-check-square"></i> : <i className="fas fa-clock"></i>}
         <span className="ListItem__TaskName">{props.taskName}</span>
-        <Button><i className="fas fa-check"></i></Button>
-        <Button><i className="fas fa-times"></i></Button>
+        <Button
+            className="ListItemButton Completed Hidden"
+        >
+            <i className="fas fa-check"></i>
+        </Button>
+        <Button
+            className="ListItemButton Delete Hidden"
+        >
+            <i className="fas fa-times"></i>
+        </Button>
     </li>
 );
 
