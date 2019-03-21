ec15b2d66f10419a00db30d8d491bf1c860a4f06
isjaki <usow.aleksey@yandex.ru>
2019-03-22 00:32:15 +0400

    converted ListItem component from functional to class based

--- src/components/List/ListItems/ListItem/ListItem.js
+++ src/components/List/ListItems/ListItem/ListItem.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../../../UI/Button/Button';
 import './ListItem.css';
 
-const listItem = props => {
+class ListItem extends Component {
+    render() {
+        return (
+            <li className="ListItem">
+                {this.props.completed ? <i className="fas fa-check-square"></i> : <i className="fas fa-clock"></i>}
+                <span className="ListItem__TaskName">{this.props.taskName}</span>
+                <Button
+                    className="ListItemButton Completed Hidden"
+                >
+                    <i className="fas fa-check"></i>
+                </Button>
+                <Button
+                    className="ListItemButton Delete Hidden"
+                >
+                    <i className="fas fa-times"></i>
+                </Button>
+            </li>
+        );
+    }
+}
 
-    return (
-       <li className="ListItem">
-            {props.completed ? <i className="fas fa-check-square"></i> : <i className="fas fa-clock"></i>}
-            <span className="ListItem__TaskName">{props.taskName}</span>
-            <Button
-                className="ListItemButton Completed Hidden"
-            >
-                <i className="fas fa-check"></i>
-            </Button>
-            <Button
-                className="ListItemButton Delete Hidden"
-            >
-                <i className="fas fa-times"></i>
-            </Button>
-        </li> 
-    );
-};
-
-export default listItem;
+export default ListItem;
