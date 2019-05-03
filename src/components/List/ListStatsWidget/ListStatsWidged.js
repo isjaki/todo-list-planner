d0a01698f22caee5e874f28c2dbc0e1575bbdb0c
isjaki <usow.aleksey@yandex.ru>
2019-05-03 18:19:56 +0400

    created dynamic display of completed and active tasks

--- src/components/List/ListStatsWidget/ListStatsWidged.js
+++ src/components/List/ListStatsWidget/ListStatsWidged.js
@@ -4,16 +4,30 @@ import Button from '../../UI/Button/Button';
 import './ListStatsWidget.css';
 
 const listStatsWidget = props => {
+    const activeTasksArray = props.tasks.filter(task => {
+        return !task.completed;
+    });
+
+    const completedTasksArray = props.tasks.filter(task => {
+        return task.completed;
+    });
+
+    const activeTasks = activeTasksArray.length;
+    const completedTasks = completedTasksArray.length;
+
     return (
         <div className="ListStatsWidget">
-            <div className="ListStatsWidget__item">All: 0</div>
-            <div className="ListStatsWidget__item">Active: 0</div>
-            <div className="ListStatsWidget__item">Completed: 0</div>
+            <div className="ListStatsWidget__item">Active: {activeTasks}</div>
+            <div className="ListStatsWidget__item">Completed: {completedTasks}</div>
             <div className="ListStatsWidget__item ListStatsWidget__item-sort">
                 <span>Sort by:</span>
                 <Button className="ListStatsWidget__Button">All</Button>
                 <Button className="ListStatsWidget__Button">Active</Button>
                 <Button className="ListStatsWidget__Button">Completed</Button>
+            </div>
+            <div className="ProgressBar">
+                <div className="ProgressBar__Done"></div>
+                <div className="ProgressBar__Left"></div>
             </div>
         </div>
     );
