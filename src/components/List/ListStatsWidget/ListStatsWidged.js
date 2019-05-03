978ee2bc7a8c3d4f2237f4411f2620b7a3dc2d72
isjaki <usow.aleksey@yandex.ru>
2019-05-03 14:29:24 +0400

    created ListStatsWidget component which will render and manage the statistics of a list and its tasks

---
+++ src/components/List/ListStatsWidget/ListStatsWidged.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './ListStatsWidget.css';
+
+const listStatsWidget = props => {
+
+    return (
+        <div className="ListStatsWidget">
+            List statistics
+        </div>
+    );
+}
+
+export default listStatsWidget;
