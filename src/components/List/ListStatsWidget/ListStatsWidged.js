0522a7aedc210bcf5415828800f42cd28620d2f8
isjaki <usow.aleksey@yandex.ru>
2019-05-03 14:57:05 +0400

    created divs for the future statistics of the active and completed tasks and Buttons in the ListStatsWidget component

--- src/components/List/ListStatsWidget/ListStatsWidged.js
+++ src/components/List/ListStatsWidget/ListStatsWidged.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import Button from '../../UI/Button/Button';
 import './ListStatsWidget.css';
 
 const listStatsWidget = props => {
 
     return (
         <div className="ListStatsWidget">
-            List statistics
+            <div>Active: 0</div>
+            <div>Completed: 0</div>
+            <div>
+                Sort by:
+                <Button>All</Button>
+                <Button>Active</Button>
+                <Button>Completed</Button>
+            </div>
         </div>
     );
 }
