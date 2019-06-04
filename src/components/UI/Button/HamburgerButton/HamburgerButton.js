5c74c6006871981068a245f912bc96f0d49d50b0
isjaki <usow.aleksey@yandex.ru>
2019-06-04 14:14:15 +0300

    created plug for HamburgerButton component

---
+++ src/components/UI/Button/HamburgerButton/HamburgerButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './HamburgerButton.css';
+
+const hamburgerButton = props => (
+    <div className="HamburgerButton">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+)
