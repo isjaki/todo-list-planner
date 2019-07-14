cb9aa7e0578d32afc260521b1c06a8b2cc3f82ca
isjaki <usow.aleksey@yandex.ru>
2019-07-14 17:32:55 +0300

    changed the AddItem component from class-based to the functional one

--- src/components/List/AddItem/AddItem.js
+++ src/components/List/AddItem/AddItem.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 import './AddItem.css';
 
-class AddItem extends Component {
-    render() {
-        const inputClasses = ['AddItemInput'];
+const addItem = props => {
+    const inputClasses = ['AddItemInput'];
 
-        if (this.props.inputError) {
-            inputClasses.push('Error');
-        }
-
-        return (
-            <Aux>
-                <input 
-                    type="text"
-                    className={inputClasses.join(' ')}
-                    placeholder={this.props.inputError ? 'You must input something!' : "What are you up to?"}
-                    value={this.props.value}
-                    onChange={this.props.onInputChange}
-                    onKeyUp={this.props.onAddListItem} />
-                <Button
-                    className="ListButton AddItem"
-                    clicked={this.props.onAddListItem}
-                >Add</Button>
-            </Aux>
-        );
+    if (props.inputError) {
+        inputClasses.push('Error');
     }
+
+    return (
+        <Aux>
+            <input 
+                type="text"
+                className={inputClasses.join(' ')}
+                placeholder={props.inputError ? 'You must input something!' : "What are you up to?"}
+                value={props.value}
+                onChange={props.onInputChange}
+                onKeyUp={props.onAddListItem} />
+            <Button
+                className="ListButton AddItem"
+                clicked={props.onAddListItem}
+            >Add</Button>
+        </Aux>
+    );
 }
 
-export default AddItem;
+export default addItem;
